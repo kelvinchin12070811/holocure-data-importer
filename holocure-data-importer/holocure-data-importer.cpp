45193952d73d5f2efa258e578a76ef6d40253108
Kelvin Chin <chinkaiwen@yahoo.com>
2022-10-23 19:18:33 +0800

    initialized project

---
+++ holocure-data-importer/holocure-data-importer.cpp
@@ -0,0 +1,12 @@
+﻿// holocure-data-importer.cpp : Defines the entry point for the application.
+//
+
+#include "holocure-data-importer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
